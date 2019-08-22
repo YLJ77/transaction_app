087c37d6b8d81bd3f47646eeb8ef659e4c416080
boom <linjieyang77@gmail.com>
2019-08-22 08:06:51 +0800

    respecting the softkeyboard insets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -38,57 +38,59 @@ class _NewTransactionState extends State<NewTransaction> {
   }
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child:
-        Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: _titleController,
-                onSubmitted: (_) => _submitData(),
+    return SingleChildScrollView(
+        child: Card(
+            child:
+            Container(
+              margin: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: _titleController,
+                    onSubmitted: (_) => _submitData(),
 /*
                 onChanged: (val) {
                   titleInput = val;
                 },
 */
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                onSubmitted: (_) => _submitData(),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    onSubmitted: (_) => _submitData(),
 //                onChanged: (val) => amountInput = val,
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(_selectedDate == null ?
-                      'No Date Chosen' :
-                      'selectdDate: ${DateFormat.yMd().format(_selectedDate)}'),
+                  ),
+                  Container(
+                    height: 70,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(_selectedDate == null ?
+                          'No Date Chosen' :
+                          'selectdDate: ${DateFormat.yMd().format(_selectedDate)}'),
+                        ),
+                        FlatButton(
+                          onPressed: _presentDatePicker,
+                          child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),),
+                          textColor: Theme.of(context).primaryColor,
+                        )
+                      ],
                     ),
-                    FlatButton(
-                      onPressed: _presentDatePicker,
-                      child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),),
-                      textColor: Theme.of(context).primaryColor,
-                    )
-                  ],
-                ),
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    onPressed: _submitData,
+                    child: Text('Add Transaction', ),
+                    textColor: Theme.of(context).textTheme.button.color,
+                  )
+                ],
               ),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                onPressed: _submitData,
-                child: Text('Add Transaction', ),
-                textColor: Theme.of(context).textTheme.button.color,
-              )
-            ],
-          ),
-        )
+            )
 
+        ),
     );
   }
 }
